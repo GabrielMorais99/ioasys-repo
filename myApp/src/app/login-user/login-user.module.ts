819e54dbb2774455e9faa2e1f7f644a480b0dff1
GabrielMorais99 <gabrielmorais80@hotmail.com>
2020-11-04 16:08:40 -0300

    'test'

--- myApp/src/app/login-user/login-user.module.ts
+++ myApp/src/app/login-user/login-user.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { LoginUserPageRoutingModule } from './login-user-routing.module';
+import { LoginUserPageRoutingModule } from './login-user-routing.module'
 
-import { LoginUserPage } from './login-user.page';
+import { LoginUserPage } from './login-user.page'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    LoginUserPageRoutingModule
-  ],
-  declarations: [LoginUserPage]
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		ReactiveFormsModule,
+		LoginUserPageRoutingModule,
+	],
+	declarations: [LoginUserPage],
 })
 export class LoginUserPageModule {}
--- myApp/src/app/login-user/login-user.page.ts
+++ myApp/src/app/login-user/login-user.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-login-user',
-  templateUrl: './login-user.page.html',
-  styleUrls: ['./login-user.page.scss'],
+	selector: 'app-login-user',
+	templateUrl: './login-user.page.html',
+	styleUrls: ['./login-user.page.scss'],
 })
 export class LoginUserPage implements OnInit {
+	public loginForm: FormGroup
+	messageCPF = ''
+	messageDATAnasc = ''
+	errorEmail = false
+	errorSenha = false
 
-  constructor() { }
+	constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.loginForm = this.formBuilder.group({
+			Email: [
+				'',
+				Validators.compose([
+					Validators.minLength(14),
+					Validators.required,
+				]),
+			],
 
+			Senha: [
+				'',
+				Validators.compose([
+					Validators.minLength(10),
+					Validators.required,
+				]),
+			],
+		})
+	}
+
+	async login() {}
 }
